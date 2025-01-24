9d9652484ee54607ed219f8cc43bb8c51df1a036
Haakon <tveter90@gmail.com>
2025-01-25 00:13:33 +0100

    .....

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,6 +1,22 @@
+// Core Node.js polyfills
+import { Buffer } from 'buffer';
+import process from 'process/browser';
+
+// Browser polyfills
 import 'url-polyfill';
-import 'buffer/';
-import 'process/browser';
+
+// Additional Node.js polyfills
+if (typeof window !== 'undefined') {
+    window.Buffer = window.Buffer || Buffer;
+    window.process = window.process || process;
+
+    // Polyfill other Node.js globals that might be needed
+    window.global = window;
+    window.global.Buffer = window.Buffer;
+    window.global.process = window.process;
+}
+
+// Import other necessary polyfills
 import 'stream-browserify';
 import 'util/';
 import 'path-browserify';
